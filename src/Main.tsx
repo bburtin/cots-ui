fe40ee2390532407f3eed4ba717d5e92f22a9397
Boris Burtin <boris@burtin.net>
2023-10-14 10:00:44 -0700

    Initial home page
    Add an initial version of the home page with text boxes
    and buttons for viewing and administering a game.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,63 @@
+import { useState } from 'react';
+
+import { CssVarsProvider } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+
+const horizontalSx = {
+  display: 'flex',
+  gap: 2
+};
+
 function Main() {
-  return <h1>Main</h1>;
+  const [viewId, setViewId] = useState('');
+  const [adminId, setAdminId] = useState('');
+
+  function handleChangeViewId(e: React.ChangeEvent<HTMLInputElement>) {
+    setViewId(e.target.value);
+  }
+
+  function handleChangeAdminId(e: React.ChangeEvent<HTMLInputElement>) {
+    setAdminId(e.target.value);
+  }
+
+  function handleClickViewGame(e: React.MouseEvent<HTMLButtonElement>) {
+    console.log(`Viewing game ${viewId}`);
+  }
+
+  function handleClickAdminGame(e: React.MouseEvent<HTMLButtonElement>) {
+    console.log(`Administering game ${adminId}`);
+  }
+
+  return (
+    <CssVarsProvider>
+      <h1>Check Out the Score!</h1>
+      <Sheet sx={horizontalSx}>
+        <FormControl orientation="horizontal">
+          <FormLabel>View game</FormLabel>
+          <Input
+            name="viewId"
+            onChange={handleChangeViewId}
+            endDecorator={<Button onClick={handleClickViewGame}>+</Button>}
+          />
+        </FormControl>
+      </Sheet>
+
+      <Sheet sx={horizontalSx}>
+        <FormControl orientation="horizontal">
+          <FormLabel>Administer game</FormLabel>
+          <Input
+            name="adminId"
+            onChange={handleChangeAdminId}
+            endDecorator={<Button onClick={handleClickAdminGame}>+</Button>}
+          />
+        </FormControl>
+      </Sheet>
+    </CssVarsProvider>
+  );
 }
 
 export default Main;
