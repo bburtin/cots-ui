be0dbf529bf4d9a513635259d2db22a8f9a36282
Boris Burtin <boris@burtin.net>
2023-10-22 20:38:08 -0700

    Create a game on the main page
    Add a form to **Main.tsx** that allows the user to create a new game.
    When the user submits the form, we post the game and team names to
    the API and redirect to the admin page.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,25 +1,70 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 import { CssVarsProvider } from '@mui/joy/styles';
 import Sheet from '@mui/joy/Sheet';
 import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
 import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
+
+import { Game } from './models';
+import { GameResponse, gameFromResponse, getRefetchInterval } from './api';
 
 const horizontalSx = {
   display: 'flex',
   gap: 2
 };
 
+class CreateGameBody {
+  name?: string;
+  team1_name: string;
+  team2_name: string;
+
+  constructor(team1Name: string, team2Name: string) {
+    this.team1_name = team1Name;
+    this.team2_name = team2Name;
+  }
+};
+
+async function createGame(body: CreateGameBody): Promise<Game> {
+  console.log(`Creating a game: ${JSON.stringify(body)}`);
+  const response = await axios.post<GameResponse>('/api/v1/games', body);
+  console.log(`Received status ${response.status}, ${JSON.stringify(response.data)}`)
+  return gameFromResponse(response.data);
+}
+
 function Main() {
   const [viewId, setViewId] = useState('');
   const [adminId, setAdminId] = useState('');
+  const [team1Name, setTeam1Name] = useState('');
+  const [team2Name, setTeam2Name] = useState('');
+  const [gameName, setGameName] = useState('');
+
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const mutation = useMutation(
+    (body: CreateGameBody) => createGame(body),
+    {
+      onSuccess: (game, variables, context) => {
+        const key = ['games', game.id];
+        queryClient.setQueryData(key, game);
+        navigate(`/admin/${game.adminId}`);
+      }
+    }
+  );
 
   function handleChangeViewId(e: React.ChangeEvent<HTMLInputElement>) {
     const upper = (e.target.value || '').toUpperCase();
     setViewId(upper.slice(0, 4));
+  }
+
+  function handleSubmitViewGame(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    navigate(`/view/${viewId}`);
   }
 
   function handleChangeAdminId(e: React.ChangeEvent<HTMLInputElement>) {
@@ -27,12 +72,31 @@ function Main() {
     setAdminId(upper.slice(0, 4));
   }
 
-  function handleSubmitViewGame(e: React.FormEvent<HTMLFormElement>) {
-    navigate(`/view/${viewId}`);
+  function handleSubmitAdminGame(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    navigate(`/admin/${adminId}`);
   }
 
-  function handleSubmitAdminGame(e: React.FormEvent<HTMLFormElement>) {
-    navigate(`/admin/${adminId}`);
+  function handleChangeTeam1Name(e: React.ChangeEvent<HTMLInputElement>) {
+    setTeam1Name(e.target.value);
+  }
+
+  function handleChangeTeam2Name(e: React.ChangeEvent<HTMLInputElement>) {
+    setTeam2Name(e.target.value);
+  }
+
+  function handleChangeGameName(e: React.ChangeEvent<HTMLInputElement>) {
+    setGameName(e.target.value);
+  }
+
+  function handleSubmitCreateGame(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log('Creating a game');
+    const body = new CreateGameBody(team1Name, team2Name);
+    if (gameName) {
+      body.name = gameName;
+    }
+    mutation.mutate(body);
   }
 
   return (
@@ -66,6 +130,47 @@ function Main() {
                 <Button type="submit">Administer</Button>
               }
             />
+          </FormControl>
+        </form>
+      </Sheet>
+
+      <h3>Create a game:</h3>
+      <Sheet sx={horizontalSx}>
+        <form onSubmit={handleSubmitCreateGame}>
+          <FormControl>
+            <FormLabel>Team 1</FormLabel>
+            <Input
+              name="team1Name"
+              placeholder="Team name"
+              value={team1Name}
+              onChange={handleChangeTeam1Name}
+              required
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Team 2</FormLabel>
+            <Input
+              name="team2Name"
+              placeholder="Team name"
+              value={team2Name}
+              onChange={handleChangeTeam2Name}
+              required
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Game <i>(Optional)</i></FormLabel>
+            <Input
+              name="gameName"
+              placeholder="Game name"
+              value={gameName}
+              onChange={handleChangeGameName}
+            />
+          </FormControl>
+
+          <FormControl>
+            <Button type="submit">Create</Button>
           </FormControl>
         </form>
       </Sheet>
